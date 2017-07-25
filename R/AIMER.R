5def58cdb155e0d8620238dc417802535c838dab
Daniel J. McDonald <dajmcdon@gmail.com>
2017-07-25 14:50:21 -0400

    Updating documentation.

--- R/AIMER.R
+++ R/AIMER.R
@@ -1,10 +1,25 @@
-#' aimer: Approxima
+#' aimer: Amplified, Initially Marginal, Eigenvector Regression
 #'
-#' The aimer package provides three categories of important functions:
-#' foo, bar and baz.
+#' The aimer package implements the aimer algorithm as described in [[INSERT PAPER NAME HERE]].
+#' The main goal is to use marginal regression to select a subset of coefficients, use
+#' matrix approximation to estimate the principal components, and then extend those estimates
+#' to the original predictor space before thresholding.
 #'
-#' @section Foo functions:
-#' The foo functions ...
+#' The package uses fast C++ routines to quickly perform matrix computations and cross validation for
+#' selection of tuning parameters.
+#'
+#' The package provides functions for estimating the model, choosing tuning parameters, and
+#' generating simulated data as well as methods for prediction, plotting, and extraction.
+#'
+#' @section Estimation functions
+#' list them here
+#'
+#' @section Selecting tuning parameters
+#'
+#' @section Simulation
+#'
+#' @section Methods
+#'
 #'
 #' @docType package
 #' @name aimer
