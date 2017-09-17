5cda48ec047dcb10c2545a6bade959f43205847b
mtmcbride <michmcbr@indiana.edu>
2017-09-17 15:52:22 -0400

    Fixed issues with the aimer CV function

--- src/AIMER.cpp
+++ src/AIMER.cpp
@@ -127,12 +127,10 @@ Rcpp::List findThresholdAIMER(arma::mat X, arma::colvec y, arma::colvec ncomps,
         stop = (k + 1)*foldSize - 1;
         TrainTest tt = ttsplit(X, y, start, stop);
         arma::colvec tStats = arma::abs(marginalRegressionTT(tt.Xtrain, tt.ytrain));
-        arma::colvec sortedT = arma::sort(tStats);
+        arma::uvec indices = arma::sort_index(tStats);
         for(int i = 0; i < nthresh; i++){   //tries different number of nCovs[i] highest marginal correlations to accept
-            double threshold = findThresh(sortedT, nCovs[i]);                 //TODO: (after trying partial svd to speed up) move declarations outside of for loop
-            IndexSplit parti = partition(tStats, threshold);  //      -add comments for for loops
-            arma::mat Xnew = X.cols(parti.indices);
-            arma::mat F = arma::trans(Xnew) * Xnew.cols(0, parti.split - 1);
+            arma::mat Xnew = X.cols(indices);
+            arma::mat F = arma::trans(Xnew) * Xnew.cols(Xnew.n_cols - nCovs(i), Xnew.n_cols);
             if(F.n_cols > F.n_rows){
                 return Rcpp::List::create(Rcpp::Named("Error") = "F has more columns than rows",
                                           Rcpp::Named("columns") = F.n_cols,
@@ -166,7 +164,7 @@ Rcpp::List findThresholdAIMER(arma::mat X, arma::colvec y, arma::colvec ncomps,
                 }
                 arma::mat Ud = Xnew * VSInv;
                 arma::colvec beta = VSInv * trans(Ud) * tt.ytrain;
-                arma::mat testX = tt.Xtest.cols(partition(tStats, threshold).indices);
+                arma::mat testX = tt.Xtest.cols(indices);
                 arma::colvec Yhat = testX * beta;
                 CVmse(i, j, k) = arma::mean(arma::square((tt.ytest - Yhat)));
             }
@@ -237,7 +235,7 @@ arma::colvec AIMER(arma::mat X, arma::colvec y,
     arma::colvec beta = VSInv * trans(Ud) * y;
     arma::uvec betaSortedIndices = invert(arma::sort_index(arma::abs(beta)));
     for(int i = 0; i < beta.n_elem; i++){
-        if(betaSortedIndices(i) < (beta.n_elem - nCovsSelect)){     //abs() only worked for integers
+        if(betaSortedIndices(i) < (beta.n_elem - nCovsSelect)){
             beta(i) = 0;
         }
     }
@@ -268,12 +266,10 @@ Rcpp::List findThresholdSel(arma::mat X, arma::colvec y, arma::colvec ncomps,
         stop = (k + 1)*foldSize - 1;
         TrainTest tt = ttsplit(X, y, start, stop);
         arma::colvec tStats = arma::abs(marginalRegressionTT(tt.Xtrain, tt.ytrain));
-        arma::colvec sortedT = arma::sort(tStats);
+        arma::uvec indices = arma::sort_index(tStats);
         for(int i = 0; i < nthresh; i++){   //tries different number nCovs[i] highest marginal correlations to use
-            double threshold = findThresh(sortedT, nCovs[i]);
-            IndexSplit parti = partition(tStats, threshold);
-            arma::mat Xnew = tt.Xtrain.cols(parti.indices);
-            arma::mat F = arma::trans(Xnew) * Xnew.cols(0, parti.split - 1);
+            arma::mat Xnew = tt.Xtrain.cols(indices);
+            arma::mat F = arma::trans(Xnew) * Xnew.cols(Xnew.n_cols - nCovs(i), Xnew.n_cols - 1);
             if(F.n_cols > F.n_rows){
                 return Rcpp::List::create(Rcpp::Named("Error") = "F has more columns than rows",
                                           Rcpp::Named("columns") = F.n_cols,
@@ -310,14 +306,14 @@ Rcpp::List findThresholdSel(arma::mat X, arma::colvec y, arma::colvec ncomps,
                 //Select process
                 for(int l = 0; l < nthreshSelect; l++){    //tries different number nCovsSelect(l) largest betas to keep
                     arma::colvec beta = initialBeta;
-                    double bthresh = findThresh(arma::sort(arma::abs(beta)), nCovsSelect(l));
+                    arma::uvec sortedBetaIndices = invert(arma::sort_index(arma::abs(beta)));
                     for(int m = 0; m < beta.n_elem; m++){   //sets the remaining betas to zero
-                        if(beta(m) < bthresh && -1*beta(m) < bthresh){     //abs() only worked for integers
+                        if(sortedBetaIndices(m) < (beta.n_elem - nCovsSelect(l))){     //abs() only worked for integers
                             beta(m) = 0;
                         }
                     }
                     //no straightforward way to boolean index, so I use the whole test matrix and beta vector with zeros.
-                    arma::mat testX = tt.Xtest.cols(partition(tStats, threshold).indices);
+                    arma::mat testX = tt.Xtest.cols(indices);
                     arma::colvec Yhat = testX * beta;
                     if(k == 0){
                         CVmse(l, j, i) = arma::colvec(kfold);
@@ -348,21 +344,21 @@ Rcpp::List findThresholdSel(arma::mat X, arma::colvec y, arma::colvec ncomps,
             }
         }
     }
-    double threshold = findThresh(arma::sort(arma::abs(marginalRegressionTT(X, y))), nCovs[bestnthresh]);
-    arma::colvec beta = AIMER(X, y, threshold, 0, ncomps[bestncomp]);
+    //double threshold = findThresh(arma::sort(arma::abs(marginalRegressionTT(X, y))), nCovs[bestnthresh]);
+    arma::colvec beta = AIMER(X, y, nCovs[bestnthresh], nCovsSelect[bestnthreshSelect], ncomps[bestncomp]);
     double bthresh = findThresh(arma::sort(arma::abs(beta)), nCovsSelect[bestnthreshSelect]);
-    for(int m = 0; m < beta.n_elem; m++){   //sets the remaining betas to zero
+    /*for(int m = 0; m < beta.n_elem; m++){   //sets the remaining betas to zero
         if(beta(m) < bthresh && -1*beta(m) < bthresh){     //abs() only worked for integers
             beta(m) = 0;
         }
-    }
+    }*/
     return Rcpp::List::create(Rcpp::Named("nCov.select.best") = nCovsSelect[bestnthreshSelect],
                               Rcpp::Named("ncomp.best") = ncomps[bestncomp],
                               Rcpp::Named("nCov.best") = nCovs[bestnthresh],
                               Rcpp::Named("ncomps") = ncomps,
                               Rcpp::Named("nCovs") = nCovs,
                               Rcpp::Named("nCovs.select") = nCovsSelect,
-                              Rcpp::Named("threshold") = threshold,
+                              //Rcpp::Named("threshold") = threshold,
                               Rcpp::Named("bthreshold") = bthresh,
                               //Rcpp::Named("CVmse") = CVmse,       //Rcpp can't return this
                               Rcpp::Named("mse") = mse,
